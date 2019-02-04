ee4965d222c83dcceb5a1c6de3a80f5867afcfbb
PaulyMac <36276390+PaulyMac@users.noreply.github.com>
2019-02-04 15:35:16 +0000

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +9,23 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     remember_me = BooleanField("Remember Me")
     submit = SubmitField("Sign In")
+class RegisterForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequire()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirmpass = PasswordField("Confirm_Password", validators=[DataRequired()])
+    submit = SubmitField("Register")
+    def validate_passwordmatch(self,password,confirmpass):
+        pass1 = password.data
+        pass2 = confirmpass.data
+        if pass1 != pass2:
+            raise ValidationError("Please make sure the passwords match")
+
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError("Please use a different username")
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError("Please use a different email address.")    
