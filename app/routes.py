c52ff10f0d5d9e0d33c65e82e0709cc97d3a5fba
PaulyMac <36276390+PaulyMac@users.noreply.github.com>
2019-02-06 13:14:51 +0000

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -23,7 +23,19 @@ def index():
     ]
     return render_template("index.html", title="Home", user=user, posts=posts)
 
-
+@app.route("/register", methods=["GET","POST"])
+def register():
+if current_user.is_authenticated:
+            return redirect(url_for("index"))
+        form = RegistrationForm()
+        if form.validate_on_submit():
+            user = User(username=form.username.data, email=form.email.data)
+            user.set_password(form.password.data)
+            db.session.add(user)
+            db.session.commit()
+            flash("Succesfully Registered")
+            return redirect(url_for("index"))
+        return render_template("register.html", title="Register", form = form)
 @app.route('/login', methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
